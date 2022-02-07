16284da2ef93a78f0019fead9d7841acad8c079e
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-02-07 16:37:43 +0100

    Create Home page and use it in the app component

---
+++ src/components/Homepage/home.js
@@ -0,0 +1,12 @@
+// import { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+//  import { fetchMission } from '../redux/Missions/Missions';
+
+const Home = () => {
+  const hi = 'hi';
+  return (
+    <h1>{hi}</h1>
+  );
+};
+
+export default Home;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,4 @@
 // configureStore.js
-
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
