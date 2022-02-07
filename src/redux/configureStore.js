7232d431db70b670a23cf1559b75b273fe063722
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-02-07 15:44:22 +0100

    configure Store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+// configureStore.js
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import covidReducer from './covidCases/cases';
+
+const reducer = combineReducers({ covidReducer });
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger, thunk),
+);
+
+export default store;
--- src/redux/covidCases/cases.js
+++ src/redux/covidCases/cases.js
@@ -16,3 +16,5 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export default reducer;
