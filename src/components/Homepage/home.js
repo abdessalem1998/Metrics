355efece5704f50ab0cf02cfc56b9a9c3ffaf0f3
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-02-08 15:27:27 -0800

    Fetch data from api and display it in the home page

--- src/components/Homepage/home.js
+++ src/components/Homepage/home.js
@@ -1,14 +1,47 @@
-// import { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-//  import { fetchMission } from '../redux/Missions/Missions';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCases } from '../../redux/covidCases/cases';
 import Filter from './Homeitems/Filter';
+import Item from './Homeitems/Item';
 
 const Home = () => {
-  const hi = 'find';
+  const covidData = useSelector((state) => state.covidReducer);
+  const dispatch = useDispatch();
+
+  const today = new Date().toISOString().slice(0, 10);
+  const URL = 'https://api.covid19tracking.narrativa.com/api';
+
+  const fetchData = async () => {
+    const response = await fetch(`${URL}/${today}`, {
+      method: 'GET',
+    });
+    const jsonRespnse = await response.json();
+    const neededData = jsonRespnse.dates[`${today}`].countries;
+    dispatch(fetchCases(Object.values(neededData)));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
-      <Filter />
-      {hi}
+      <div className="row">
+        <Filter />
+        {
+          covidData.map((singledata) => (
+            <Item
+              key={singledata.id}
+              name={singledata.name}
+              date={singledata.date}
+              totalConfirmed={singledata.today_confirmed}
+              totalDeaths={singledata.today_deaths}
+              dateConfirmed={singledata.today_new_confirmed}
+              dateDeaths={singledata.today_new_deaths}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 };
