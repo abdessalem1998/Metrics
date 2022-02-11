95edadc5cbfe06699c94a89a9344c0e3ad1101d5
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-02-10 17:38:52 -0800

    Fix linters

--- src/components/Homepage/Homeitems/Filter.js
+++ src/components/Homepage/Homeitems/Filter.js
@@ -1,30 +1,47 @@
-/* eslint-disable */
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { filterCasesByName } from '../../../redux/covidCases/cases';
 import world from './world.svg';
 
 const Filter = (data) => {
-  const covidData = useSelector((state) => state.covidReducer);
   const dispatch = useDispatch();
   const filterHandler = (e) => {
     dispatch(filterCasesByName(e));
   };
+  /* eslint-disable react/destructuring-assignment */
   return (
-    <div className="input-group mb-5 text-center text-white" style={{height:'250px'}}>
-    <div className="col-6 d-flex align-items-center" style={{backgroundImage: `url(${world})`,backgroundSize: '100%'}}>
-      <input type="text" onChange={(e) => filterHandler(e.target.value)} className="form-control border border-white" placeholder="Find ..." />
-    </div>
-      <div className="col-6 ">
-        <h2></h2>
-        <h2>World Cases </h2>
-        <p><b>Total confirmed cases: {data.data.today_confirmed}</b></p>
-        <p><b>Total deaths: {data.data.today_deaths}</b></p>
-        <p><b>{data.data.date} cases: {data.data.today_new_confirmed}</b></p>
-        <p><b>{data.data.date} deaths: {data.data.today_new_deaths}</b></p>
+    <div className="input-group mb-5 text-center text-white" style={{ height: '250px' }}>
+      <div className="col-6 d-flex align-items-center" style={{ backgroundImage: `url(${world})`, backgroundSize: '100%' }}>
+        <input type="text" onChange={(e) => filterHandler(e.target.value)} className="form-control border border-white" placeholder="Find ..." />
       </div>
-
-
+      <div className="col-6 ">
+        <h2>World Cases </h2>
+        <p>
+          <b>
+            Total confirmed cases:
+            {data.data.today_confirmed}
+          </b>
+        </p>
+        <p>
+          <b>
+            Total deaths:
+            {data.data.today_deaths}
+          </b>
+        </p>
+        <p>
+          <b>
+            {data.data.date}
+            cases:
+            {data.data.today_new_confirmed}
+          </b>
+        </p>
+        <p>
+          <b>
+            {data.data.date}
+            deaths:
+            {data.data.today_new_deaths}
+          </b>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Homepage/home.js
+++ src/components/Homepage/home.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCases } from '../../redux/covidCases/cases';
@@ -28,7 +27,6 @@ const Home = () => {
     });
     const jsonRespnse = await response.json();
     const neededData = jsonRespnse.dates[`${today}`].countries;
-    //console.log(jsonRespnse.total);
     setWorldData(jsonRespnse.total);
     dispatch(fetchCases(Object.values(neededData)));
   };
@@ -38,64 +36,61 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container-fluid " style={{backgroundColor:'#FC5193' }}>
+    <div className="container-fluid " style={{ backgroundColor: '#FC5193' }}>
       <div className="row">
-        <Nav year="2022" title="Whole World Data"/>
-        <Filter data={worldData}/>
-        <p className="text-white m-0 p-1" style={{backgroundColor:'#DD4883' }}><b>STATS BY COUNTRY</b></p>
+        <Nav year="2022" title="Whole World Data" />
+        <Filter data={worldData} />
+        <p className="text-white m-0 p-1" style={{ backgroundColor: '#DD4883' }}><b>STATS BY COUNTRY</b></p>
         {
+          /* eslint-disable array-callback-return */
+          /* eslint-disable consistent-return */
           covidData.map((singledata) => {
             if (!singledata.filtered) {
-              countDesign++;
-              if (countDesign ===1) {
-                color1='#EA4C89'
-              }else {
+              countDesign += 1;
+              if (countDesign === 1) {
+                color1 = '#EA4C89';
+              } else {
+                /* eslint-disable no-lonely-if */
                 if (switcher < 2) {
-                  color1='#CE4176'
-                  switcher++;
-                }else {
-                  if (switcher<4) {
-                    switcher++;
-                    color1='#EA4C89'
-                  }else {
-                    color1='#CE4176'
-                    switcher=1;
+                  color1 = '#CE4176';
+                  switcher += 1;
+                } else {
+                  if (switcher < 4) {
+                    switcher += 1;
+                    color1 = '#EA4C89';
+                  } else {
+                    color1 = '#CE4176';
+                    switcher = 1;
                   }
-
                 }
               }
 
               return (
-              <div key={singledata.id} className="col-6 text-white m-0" style= {{backgroundColor: `${color1}`}}>
-              <Link
-              style={{backgroundColor:'red' }}
-              className="text-white"
-              state={singledata}
-              color={color1}
-              to={`/details/${singledata.name}`}
-              style={{ textDecoration: 'none',color:'black' }}
-              >
-              <div className="row" >
-                <div className="col-4 " style={{backgroundImage: `url(${covid})`,backgroundRepeat:'no-repeat',backgroundSize: '60%'}}>
+                <div key={singledata.id} className="col-6 text-white m-0" style={{ backgroundColor: `${color1}` }}>
+                  <Link
+                    className="text-white"
+                    state={singledata}
+                    color={color1}
+                    to={`/details/${singledata.name}`}
+                    style={{ textDecoration: 'none' }}
+                  >
+                    <div className="row">
+                      <div className="col-4" style={{ backgroundImage: `url(${covid})`, backgroundRepeat: 'no-repeat', backgroundSize: '60%' }} />
+                      <Item
+                        name={singledata.name}
+                        date={singledata.date}
+                        totalConfirmed={singledata.today_confirmed}
+                        totalDeaths={singledata.today_deaths}
+                        dateConfirmed={singledata.today_new_confirmed}
+                        dateDeaths={singledata.today_new_deaths}
+                        filtered={singledata.filtered}
+                      />
+                    </div>
+                  </Link>
                 </div>
-                <Item
-                  name={singledata.name}
-                  date={singledata.date}
-                  totalConfirmed={singledata.today_confirmed}
-                  totalDeaths={singledata.today_deaths}
-                  dateConfirmed={singledata.today_new_confirmed}
-                  dateDeaths={singledata.today_new_deaths}
-                  filtered={singledata.filtered}
-                />
-
-              </div>
-
-
-              </Link>
-              </div>
-            )
+              );
             }
-            })
+          })
         }
       </div>
     </div>
